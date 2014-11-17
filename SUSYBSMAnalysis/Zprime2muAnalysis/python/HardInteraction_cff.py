f7d2b28643789af9fa704af40b27cb7019e82a5b
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2014-11-17 18:16:50 +0100

    Copied from cms-analysis

---
+++ SUSYBSMAnalysis/Zprime2muAnalysis/python/HardInteraction_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+hardInteraction = cms.PSet(src = cms.InputTag('prunedMCLeptons'),
+                           doingElectrons = cms.bool(False),
+                           allowFakeResonance = cms.bool(True),
+                           resonanceIds = cms.vint32(32, 23, 39, 5000039),
+                           shutUp = cms.bool(False),
+                           )
